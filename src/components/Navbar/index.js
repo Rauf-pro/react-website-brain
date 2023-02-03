01c17f753b98d10229342f4b92c5c12ab8af01f4
rrauf8178 <rrauf8178@gmail.com>
2023-02-04 01:05:33 +0400

    installed react-icons and created navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,16 @@
 import React from "react";
-import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from "./NavbarElements";
-import { FaBars } from 'react-icons/fa';
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink
+} from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -13,9 +23,21 @@ const Navbar = () => {
           </MobileIcon>
           <NavMenu>
             <NavItem>
-              <NavLinks to="about"></NavLinks>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
             </NavItem>
           </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="signin">Sign In</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
